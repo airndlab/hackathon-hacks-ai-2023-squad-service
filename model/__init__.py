d79184d557761add99fc80da831d396830269679
maslov <maslov@iac.spb.ru>
2023-08-24 21:28:30 +0300

    create env config file

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
+from dotenv import dotenv_values
 
-tokenizer = AutoTokenizer.from_pretrained("./model/tokenizer_ru_bert/")
-model = AutoModelForQuestionAnswering.from_pretrained("./model/pretrained_ru_bert_sbersquad/")
+config = dotenv_values("./config/.env")
+tokenizer = AutoTokenizer.from_pretrained(config.get("model_path"))
+model = AutoModelForQuestionAnswering.from_pretrained(config.get("tokenizer_path"))
 question_answerer = pipeline("question-answering", model=model, tokenizer=tokenizer)
