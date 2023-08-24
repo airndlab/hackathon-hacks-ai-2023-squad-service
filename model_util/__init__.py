ab9688112516749922f694d374fe5542fecc9c12
avetall87 <aleksandrov.vetall@yandex.ru>
2023-08-24 23:35:00 +0300

    fix config for model and tokinizer

--- model_util/__init__.py
+++ model_util/__init__.py
@@ -2,6 +2,6 @@ from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
 from dotenv import dotenv_values
 
 config = dotenv_values("./config/.env")
-tokenizer = AutoTokenizer.from_pretrained(config.get("model_path"))
-model = AutoModelForQuestionAnswering.from_pretrained(config.get("tokenizer_path"))
+tokenizer = AutoTokenizer.from_pretrained(config.get("tokenizer_path"))
+model = AutoModelForQuestionAnswering.from_pretrained(config.get("model_path"))
 question_answerer = pipeline("question-answering", model=model, tokenizer=tokenizer)
